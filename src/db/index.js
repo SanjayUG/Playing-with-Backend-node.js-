03146b3a427799d9846637a847064affa06d69f9
Sanjay UG <122535741+SanjayUG@users.noreply.github.com>
2024-10-03 12:58:49 +0000

    add: learnt to connect mongodb

--- src/db/index.js
+++ src/db/index.js
@@ -5,9 +5,29 @@ import { DB_NAME } from "../constants.js";
 
 const connectDB = async () => {
     try {
-
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+        // console.log(`DataBase connected !! Host: ${connectionInstance}`); // it is an object
+        console.log(`DataBase connected !! Host: ${connectionInstance.connection.host}`);
+        
     } catch(e) {
-        console.log("MongoDB connection Error: ", e);
+        console.log(`MongoDB connection FAILED: ${e}`);
         process.exit(1);
     }
 }
+
+
+export default connectDB;
+
+/*
+
+how to connect mongodb??
+sol: we need to setup network access and get the connecting string.
+
+- redirect to mongodb Atlas
+- do following on overview
+    -- setup 'network access' and 'database access'
+    -- go to 'database'(at deployment section) and 'connect' it through compass(get the connection string) 
+- go to codespace and paste 'connecting string' in .env file
+- use async...await and try...catch to connect db 
+
+*/
--- src/index.js
+++ src/index.js
@@ -1,60 +1,9 @@
-import { application } from "express";
-import mongoose from "mongoose";
-import { DB_NAME } from "./constants.js";
-import express from "express";
 
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
 
+dotenv.config({
+    path: './.env'
+})
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-const app = express();
-;(async () => {
-    try {
-        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);  // do not connect DB directly, use async...await and try...catch
-
-        app.on("error", (e) => {
-            console.log("Error:- ", e);
-            throw e;
-        })
-
-        app.listen(process.env.PORT, () => {
-            console.log(`App is listening at port ${process.env.PORT}`)
-        })
-    
-    } catch(error){
-        console.error("Error: ", error);
-        throw error;
-    }
-})()
-
+connectDB()
