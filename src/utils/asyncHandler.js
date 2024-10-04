8664364c125c35423c6c12a81a6b3d08b10d3793
Sanjay UG <122535741+SanjayUG@users.noreply.github.com>
2024-10-04 10:54:21 +0000

    add: worked on utils

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,54 @@
+
+const asyncHandler = (requestHandler) => {
+    (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next)).catch((error) => next(error))
+    }
+}
+
+export {asyncHandler}
+
+
+
+/*
+
+//using try...catch for asyncHandler
+
+const asyncHandler = (fun) => async (req, res, next) => {
+    try{
+        await fun(req, res, next)
+    } catch(error) {
+        res.status(error.code || 500).json({
+            success: false,
+            message: err.message
+        })
+    }
+} 
+
+*/
+
+
+
+
+
+
+
+
+/*
+
+const asyncHandler = (fun) => {
+    async () => {
+        
+    } 
+}
+
+OR
+
+cosnt asyncHandler = (fun) => () => {}
+
+OR
+
+const asyncHandler = (fun) => async () => {
+    
+}
+
+*/
