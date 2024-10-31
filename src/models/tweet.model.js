57477a4d2a5cbaa415c3cfb3915b611491ed6eb8
Sanjay UG <122535741+SanjayUG@users.noreply.github.com>
2024-10-31 05:17:14 +0000

    add: added models

---
+++ src/models/tweet.model.js
@@ -0,0 +1,19 @@
+
+
+
+import mongoose, {Schema, model} from "mongoose";
+
+
+const tweetSchema = new Schema({
+    content: {
+        type: String,
+        required: true
+    },
+    owner: {
+        type: Schema.Types.ObjectId,
+        ref: "User"
+    }
+}, {timeseries: true}
+)
+
+export const Tweet = mongoose.model("Tweet", tweetSchema);
